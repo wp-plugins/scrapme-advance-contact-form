74a537f0fd15acf183aac78f0fb731746b2106e7
btushar <btushar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-10 13:24:08 +0000

    adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/scrapme-advance-contact-form/trunk@1023012 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Plugin Name ===
+Tags: contact form, in site deal management, deal bar, social bar, splash image, popups
+Requires at least: 3.0.1
+Tested up to: 4.0
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Get an advance contact form for your site and offer site wide deals with additional tools like Deal Bar, Popups, Inline Popups, Social Bar, Leave intent targeting. 
+
+== Description ==
+
+Get an advance contact form for your site and offer site wide deals with additional tools like Deal Bar, Popups, Inline Popups, Social Bar, Leave intent targeting. Before activating the plugin please register an account from http://scrap.me and insert the API key here.
+
+
+== Installation ==
+
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Click on the scrap.me in admin menu and enter your Scrap.me API Key
+
+== Frequently Asked Questions ==
+
+= How to get the API key? =
+
+You can get the API key from http://scrap.me/. After registration go to settings page and get the Wordpress API Key.
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial relase.
+
+
+== Fearures ==
+
+1. Contact Form
+2. Deal bar
+3. Social Bar
+4. Inline and Side Popups
+5. Splash Image
+
+`<?php code(); // goes in backticks ?>`
---
+++ scrapme.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * @package scrap.me
+ * @version 1
+*/
+/*
+Plugin Name: scrap.me
+Plugin URI: http://scrap.me/
+Description: Get an advance contact form for your site and offer site wide deals with additional tools like Deal Bar, Popups, Inline Popups, Social Bar, Leave intent targeting. Before activating the plugin please register an account from http://scrap.me and insert the API key here.
+Version: 1.0
+Author: scrap.me
+Author URI: http://scrap.me
+License: GPLv2
+*/
+function scrapme_add_data() {
+	$scrapme_apikey = trim(get_option('scrapme_api_key'));
+	if(isset($scrapme_apikey) && strlen($scrapme_apikey) == 32){
+    	echo "<!--SCRAP.ME CODE-->".
+		"<script type='text/javascript'>var apikey = '". $scrapme_apikey ."';</script>".
+		"<script type='text/javascript' src='http://scrap.me/widget/popup/v1/script.js'></script>".
+		"<div id='scrapme' class='scrapme'></div>".
+		"<!--SCRAP.ME CODE-->";
+	}
+}
+add_action('wp_footer', 'scrapme_add_data');
+
+
+// create custom plugin settings menu
+add_action('admin_menu', 'baw_create_menu');
+
+function baw_create_menu() {
+
+	//create new top-level menu
+	add_menu_page('Scrap.me Plugin Settings', 'Scrap.me', 'administrator', __FILE__, 'baw_settings_page','');
+
+	//call register settings function
+	add_action( 'admin_init', 'register_mysettings' );
+}
+
+
+function register_mysettings() {
+	//register our settings
+	register_setting( 'baw-settings-group', 'scrapme_api_key' );
+}
+
+function baw_settings_page() {
+?>
+<div class="wrap">
+<h2>Set Scrap.me API Key</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'baw-settings-group' ); ?>
+    <?php do_settings_sections( 'baw-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">API Key</th>
+        <td><input type="text" maxlength="32" style="width: 300px;" name="scrapme_api_key" value="<?php echo esc_attr( get_option('scrapme_api_key') ); ?>" /></td>
+        </tr>
+        
+    </table>
+    <div>API Key is 32 bit long. Please do not enter any special characters or space in the key.</div>
+    <?php submit_button(); ?>
+	<div style="padding: 4px;">You can get the API key by registering from <a href="http://scrap.me/register.php" target="_blank" style="text-decoration: none">http://scrap.me/register.php</a>. To see a live demo on your website visit <a href="http://scrap.me/show_demo.html" target="_blank" style="text-decoration: none">Demo Page</a></div>
+</form>
+</div>
+<?php } ?>
